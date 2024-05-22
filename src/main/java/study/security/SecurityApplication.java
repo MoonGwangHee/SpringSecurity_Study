f1836b4d2425a94ce1020aee9fac12ba49073f70
MoonGwangHee <mgh7020@gmail.com>
2024-05-22 14:56:19 +0900

    1st Commit for main settings

---
+++ src/main/java/study/security/SecurityApplication.java
@@ -0,0 +1,15 @@
+package study.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+
+        // Java 17 버전으로 셋팅후 작업하셈
+    }
+
+}
